522d65e8cdbdea4d8e38eaced1a9becdd7404514
Luo ju <1521152077@qq.com>
2024-01-16 21:43:33 +0800

    x

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 // import HomePage from 'home/pages/index';
-const HomePage = () => <div>HomePage</div>
+const HomePage = () => <div>HomePage1</div>
 console.log('homepage', HomePage)
 const Home = HomePage;
 Home.getInitialProps = HomePage.getInitialProps;
